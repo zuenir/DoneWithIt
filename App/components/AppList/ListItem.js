ce834d8b651a3aaa35c27da909993655434a04cd
zuenir <zuenirlima@gmail.com>
2020-10-30 10:36:19 -0300

    1. Getting Started 2. Fundamental Concepts 3. Layout 4. Styling 5. Lists

--- App/components/AppList/ListItem.js
+++ App/components/AppList/ListItem.js
@@ -4,7 +4,7 @@ import colors from '../../config/colors';
 import AppText from '../AppText/AppText';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 
-function ListItem({title, subTitle, image, onPress, renderRightActions}) {
+function ListItem({title, subTitle, image,ImageComponent, onPress, renderRightActions}) {
     return (
         <Swipeable
             renderRightActions={renderRightActions}>
@@ -12,10 +12,11 @@ function ListItem({title, subTitle, image, onPress, renderRightActions}) {
                 underlayColor = {colors.light}
                 onPress = {onPress}>
                 <View style={styles.container}>
-                    <Image style={styles.image} source={image}/>
-                    <View>
+                    {ImageComponent}
+                    {image && <Image style={styles.image} source={image}/>}
+                    <View style={styles.detailsContainer}>
                         <AppText style={styles.title}>{title}</AppText>
-                        <AppText style={styles.subTitle}>{subTitle}</AppText>
+                        {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
                     </View>
                 </View>
             </TouchableHighlight>
@@ -27,11 +28,14 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         padding: 15,
     },
+    detailsContainer:{
+        marginLeft: 15,
+        justifyContent: "center"
+    },
     image: {
         width: 70,
         height: 70,
         borderRadius: 35,
-        marginRight: 10,
     },
     subTitle: {
         color:colors.medium,
